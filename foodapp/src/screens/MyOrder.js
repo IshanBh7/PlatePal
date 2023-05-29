87d7a63905a2d6d9c2c72158c43bce29d609c68b
Ishan <cristianogrzmnn@gmail.com>
2023-05-30 05:08:22 +0530

    deploy changes

--- foodapp/src/screens/MyOrder.js
+++ foodapp/src/screens/MyOrder.js
@@ -8,7 +8,7 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => {
         // console.log(localStorage.getItem('userEmail'))
-        await fetch("http://localhost:5000/api/myOrderData", {
+        await fetch("plate-pal-99tg-fgqu257v7-ishanbh7.vercel.app/api/myOrderData", {
             // credentials: 'include',
             // Origin:"http://localhost:3000/login",
             method: 'POST',
