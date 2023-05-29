09a3111aadd1e3de4e861920fcfb66ddf935e129
Ishan <cristianogrzmnn@gmail.com>
2023-05-30 03:25:22 +0530

    deployment changes

--- foodapp/backend/index.js
+++ foodapp/backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT||5000;
 const mongoDB= require("./db")
+const path= require("path");
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
