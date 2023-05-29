39df48b321d0b72ad93c78a16268d8fc3fb9c6f7
Ishan <cristianogrzmnn@gmail.com>
2023-05-30 03:16:21 +0530

    Deployment changes

--- foodapp/backend/index.js
+++ foodapp/backend/index.js
@@ -26,12 +26,9 @@ app.listen(port, () => {
 
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static("build"));
-  app.use('/static', express.static(path.join(__dirname, 'public')))
-
+  
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "build/index.html"));
     
-   app.use(express.static('public'));
-    app.use('/images', express.static('images'));
-  });
+  })
 }
